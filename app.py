a604bbbf85b4d614a167cd3a954ff64e5815a1c3
rudberga <rudberg@pm.me>
2021-02-10 03:46:34 +0000

    ADD - app configs to connect db to app

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
 @app.route("/")
 def hello():
     return 'Hello World!'
