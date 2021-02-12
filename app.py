a397e73407cd4059909897e0f216d07049316bf2
rudberga <rudberg@pm.me>
2021-02-12 02:58:33 +0000

    ADD - sign up form and template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 @app.route("/get_home")
 def get_home():
     return render_template("index.html")
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    return render_template("sign_up.html")
 
 
 @app.route("/get_artists")
