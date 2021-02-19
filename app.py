cdad04181c4a422016361a69e764852cf1686de5
rudberga <rudberg@pm.me>
2021-02-19 08:43:26 +0000

    DELETE - flash message for logging in, not necessary

--- app.py
+++ app.py
@@ -75,7 +75,6 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
                     return redirect(url_for(
                         "profile",username=session["user"]))
             else:
