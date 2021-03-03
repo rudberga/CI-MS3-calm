91d27759b4373e2e72c9e3d3ef96331da372038b
rudberga <rudberg@pm.me>
2021-03-03 17:37:31 +0000

    ADD - users added artist cards show up on profile page

--- app.py
+++ app.py
@@ -96,12 +96,14 @@ def login():
 # Profile page functionality
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    artists = list(mongo.db.artists.find())
     # Grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template(
+            "profile.html", username=username, artists=artists)
 
     return redirect(url_for("login"))
 
